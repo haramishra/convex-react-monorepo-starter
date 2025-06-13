cce3fd12f46ebadf53b94d89a83f01d1e2d19f54
hara <haraprasad45@gmail.com>
2025-06-13 14:25:41 +0530

    lint config updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+  },
   extends: ["@workspace/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
   },
-}
+};
