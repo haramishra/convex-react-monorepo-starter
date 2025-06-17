860083be3b97ce6d6c6254e31f23a0c40d126ea8
hara <haraprasad45@gmail.com>
2025-06-18 04:20:12 +0530

    nav length fixed

--- apps/web/src/components/nav/links.tsx
+++ apps/web/src/components/nav/links.tsx
@@ -103,7 +103,7 @@ export function NavigationMenuDemo() {
                   {item.value.title}
                 </NavigationMenuTrigger>
                 <NavigationMenuContent>
-                  <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-1 lg:w-[600px]">
+                  <ul className="grid w-[300px] gap-2 md:w-[200px] md:grid-cols-1 ">
                     {item.value.items.map((dropdownItem) => (
                       <ListItem
                         key={dropdownItem.title}
